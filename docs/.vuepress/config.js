2546c79b81a15ff98e84c01fab2d24a520fc01e5
jyking <jyking@qq.com>
2023-03-29 21:10:38 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,20 @@
+import { defaultTheme } from 'vuepress'
+
 module.exports = {
-    base: '/openai-doc/'
+    base: '/openai-doc/',
+    locales: {
+        '/': {
+            lang: 'zh-CN',
+            title: 'OpenAI 中文文档',
+            description: 'OpenAI 中文文档',
+            selectLanguageName: '111',
+
+        },
+        '/en/': {
+            lang: 'en-US',
+            title: 'OpenAI docs',
+            description: 'OpenAI docs',
+            selectLanguageName: 'English',
+        }
+    },
 }
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,1 @@
-index
+cn-index
