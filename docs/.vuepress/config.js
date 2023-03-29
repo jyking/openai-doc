3c51f55ad91c9c82c952a7df4d9107d964abe2b4
jyking <jyking@qq.com>
2023-03-29 21:35:41 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,19 +2,24 @@ import { defaultTheme } from 'vuepress'
 
 module.exports = {
     base: '/openai-doc/',
+    lang: 'zh-CN',
+    title: 'OpenAI 中文文档',
+    description: 'OpenAI 中文文档',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ],
     locales: {
         '/': {
             lang: 'zh-CN',
             title: 'OpenAI 中文文档',
             description: 'OpenAI 中文文档',
-            selectLanguageName: '111',
-
-        },
-        '/en/': {
-            lang: 'en-US',
-            title: 'OpenAI docs',
-            description: 'OpenAI docs',
-            selectLanguageName: 'English',
+            selectLanguageName: '简体中文',
         }
     },
+    themeConfig: {
+        footer: {
+          author: '作者',
+          lastUpdated: '最后更新时间',
+        },
+    }
 }
