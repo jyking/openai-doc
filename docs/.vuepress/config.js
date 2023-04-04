21e8b0f94ce305e2e7363fa331d9e2bb20e29d32
jyking <jyking@qq.com>
2023-04-04 11:44:38 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,44 @@
 import { defaultTheme } from 'vuepress'
 
+let sidebarArr = [
+    {
+        text: '起步',
+        children: [
+            { text: '介绍', link: '/zh/start/introduction/' },
+            { text: '快速开始', link: '/zh/start/quickstart/' },
+            { text: '库文件', link: '/zh/start/libraries/' },
+            { text: '模型', link: '/zh/start/models/' },
+            { text: '教程', link: '/zh/start/tutorials/' },
+        ],
+    },
+    {
+        text: '向导',
+        children: [
+            { text: '文本填充', link: '/zh/guides/completion/' },
+            { text: '聊天', link: '/zh/guides/chat/' },
+            { text: '图片', link: '/zh/guides/images/' },
+            { text: '微调', link: '/zh/guides/fine-tuning/' },
+            { text: '嵌入', link: '/zh/guides/embeddings/' },
+            { text: '语音转文本', link: '/zh/guides/speech-to-text/' },
+            { text: '适度', link: '/zh/guides/moderation/' },
+            { text: '速度限制', link: '/zh/guides/rate-limits/' },
+            { text: '错误代码', link: '/zh/guides/error-codes/' },
+            { text: '最佳实践', link: '/zh/guides/safety-best-practices/' },
+            { text: '生产最佳实践', link: '/zh/guides/production-best-practices/' },
+        ],
+    },
+    {
+        text: '聊天插件',
+        children: [
+            { text: '介绍', link: '/zh/plugins/introduction/' },
+            { text: '入门', link: '/zh/plugins/getting-started/' },
+            { text: '验证', link: '/zh/plugins/authentication/' },
+            { text: '例子', link: '/zh/plugins/examples/' },
+            { text: '生产', link: '/zh/plugins/production/' },
+        ],
+    },
+]
+
 export default {
     base: '/openai-doc/',
     lang: 'zh-CN',
@@ -57,47 +96,11 @@ export default {
                             ],
                         },
                     ],
-                    '/zh/start/': [
-                        {
-                            text: '起步',
-                            children: [
-                                { text: '介绍', link: '/zh/start/introduction/' },
-                                { text: '快速开始', link: '/zh/start/quickstart/' },
-                                { text: '库文件', link: '/zh/start/libraries/' },
-                                { text: '模型', link: '/zh/start/models/' },
-                                { text: '教程', link: '/zh/start/tutorials/' },
-                            ],
-                        },
-                        {
-                            text: '向导',
-                            children: [
-                                { text: '文本填充', link: '/zh/guides/completion/' },
-                                { text: '聊天', link: '/zh/guides/chat/' },
-                                { text: '图片', link: '/zh/guides/images/' },
-                                { text: '微调', link: '/zh/guides/fine-tuning/' },
-                                { text: '嵌入', link: '/zh/guides/embeddings/' },
-                                { text: '语音转文本', link: '/zh/guides/speech-to-text/' },
-                                { text: '适度', link: '/zh/guides/moderation/' },
-                                { text: '速度限制', link: '/zh/guides/rate-limits/' },
-                                { text: '错误代码', link: '/zh/guides/error-codes/' },
-                                { text: '最佳实践', link: '/zh/guides/safety-best-practices/' },
-                                { text: '生产最佳实践', link: '/zh/guides/production-best-practices/' },
-                            ],
-                        },
-                        {
-                            text: '聊天插件',
-                            children: [
-                                { text: '介绍', link: '/zh/plugins/introduction/' },
-                                { text: '入门', link: '/zh/plugins/getting-started/' },
-                                { text: '验证', link: '/zh/plugins/authentication/' },
-                                { text: '例子', link: '/zh/plugins/examples/' },
-                                { text: '生产', link: '/zh/plugins/production/' },
-                            ],
-                        },
-                    ],
+                    '/zh/start/': sidebarArr,
+                    '/zh/guides/': sidebarArr,
+                    '/zh/plugins/': sidebarArr,
                 }
             },
         },
-
     }),
 }
